15a84d07a054ace23506c0b7843a9ef839773fbd
Johannes Röring <mail@jroering.com>
2020-11-18 10:45:32 +0100

    added GPIO Init

---
+++ main_sketch/GPIO_Definitions.ino
@@ -0,0 +1,63 @@
+/**
+  EMAG MESSESTAND, Arduino Programm
+  Name: GPIO_Definitions
+  Purpose: Storing the pin assignments for all GPIO Pins, and initializing them in the function initGPIO();
+  Arduino IDE Version: 1.8.10
+  
+  @author Johannes Röring
+  @version 1.0.0 18/11/20
+  
+  the following scripts will all be documented in german, 
+  for international use as well as translations and questions,
+  feel free to contact me
+  
+  contact: mail@jroering.com
+*/
+
+
+//  Eingänge      
+//  --Bedienpult--      
+#define E_Anlage_EIN  2 //  Eingang vom Überwachungskontakt für K1, betätigt durch Notaus
+#define E_Bed_Automatik 12  //  Eingang vom Schalter Automatikbetrieb
+#define E_Bed_Starte_Referenzfahrt  13  //  Eingang vom Taster Referenzfahrt starten
+//  --Sensoren--      
+#define E_Ini_Referenz  3 //  Eingang vom Referenztaster
+#define E_Ini_Endschalter 4 //  Eingang von Überwachungsrelais K2 & K3, überwachen Endschalter
+//  --Sensoren - Zylinder Endlagenüberwachung--     
+#define E_Ini_Zyl_1 5 //  Eingang Überwachung Zylinder 1 eingefahren
+#define E_Ini_Zyl_2 6 //  Eingang Überwachung Zylinder 2 eingefahren
+#define E_Ini_Zyl_3 7 //  Eingang Überwachung Zylinder 3 eingefahren
+#define E_Ini_Zyl_4 8 //  Eingang Überwachung Zylinder 4 eingefahren
+#define E_Ini_Zyl_5 9 //  Eingang Überwachung Zylinder 5 eingefahren
+        
+        
+//  Ausgänge      
+//  --Motor--     
+#define A_Mot_Freigabe  A0  //  Ausgang Motorfreigabe, HIGH = Motor freigegeben
+#define A_Mot_Richtung  A1  //  Ausgang Motor Richtung, HIGH = Linkslauf
+#define A_Mot_Takt  A2  //  Ausgang Motor Takt
+//  --Relais--      
+#define A_Rly_Power 41  //  Ausgang Relais -K9, Stromversorgung Relais -K10 bis -K16
+#define A_Rly_Zyl_1 42  //  Ausgang Relais -K10, Zylinder 1 ausfahren
+#define A_Rly_Zyl_2 43  //  Ausgang Relais -K11, Zylinder 2 ausfahren
+#define A_Rly_Zyl_3 44  //  Ausgang Relais -K12, Zylinder 3 ausfahren
+#define A_Rly_Zyl_4 45  //  Ausgang Relais -K13, Zylinder 4 ausfahren
+#define A_Rly_Zyl_5 46  //  Ausgang Relais -K14, Zylinder 5 ausfahren
+#define A_Rly_Bed_Referenz  47  //  Ausgang Relais -K15, Meldeleuchte Referenzfahrt gefahren
+#define A_Rly_8 48  //  Ausgang Relais -K16, nicht verwendet
+
+/**
+  initializes all GPIO Pins to the correct pinMode (Input, Output or Input with Pullup)
+*/
+void initGPIO(){
+ pinMode(E_Anlage_EIN, INPUT_PULLUP);
+ pinMode(E_Bed_Automatik,INPUT_PULLUP);
+ pinMode(E_Bed_Starte_Referenzfahrt,INPUT_PULLUP);
+ pinMode(E_Ini_Referenz,INPUT_PULLUP);
+ pinMode(E_Ini_Endschalter,INPUT_PULLUP);
+ pinMode(E_Ini_Zyl_1,INPUT_PULLUP);
+ pinMode(E_Ini_Zyl_2,INPUT_PULLUP);
+ pinMode(E_Ini_Zyl_3,INPUT_PULLUP);
+ pinMode(E_Ini_Zyl_4,INPUT_PULLUP);
+ pinMode(E_Ini_Zyl_5,INPUT_PULLUP);
+}
