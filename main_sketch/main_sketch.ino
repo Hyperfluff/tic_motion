63b0c76ee9c27d55d52552685e185144e7de43f4
Johannes Röring <mail@jroering.com>
2020-11-18 10:25:45 +0100

    Header bearbeitet

--- main_sketch/main_sketch.ino
+++ main_sketch/main_sketch.ino
@@ -3,8 +3,10 @@
   Name: main_sketch
   Purpose: controls the hardware of the Machine
   Arduino IDE Version: 1.8.10
+  
   @author Johannes Röring
   @version 1.0.0 18/11/20
+  
   the following scripts will all be documented in german, 
   for international use as well as translations and questions,
   feel free to contact me
